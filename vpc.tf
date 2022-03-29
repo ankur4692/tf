c8b36876e64cf8e15fbb2cf09dfdd05b72573d2d
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-03-30 01:54:24 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "demo" {
-  cidr_block       = "70.70.0.0/16"
+  cidr_block       = "0.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_vpc" "demo" {
 }
 resource "aws_subnet" "demo1" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "70.70.1.0/28"
+  cidr_block = "0.0.0.0/28"
 
   tags = {
     Name = "subnet1"
@@ -16,7 +16,7 @@ resource "aws_subnet" "demo1" {
 }
 resource "aws_subnet" "demo2" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "70.70.1.16/28"
+  cidr_block = "0.0.0.0/28"
 
   tags = {
     Name = "subnet2"
